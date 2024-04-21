0ea35263706839845d89048d31a92883492153f9
Natanael Borges de Oliveira <naeelbass@gmail.com>
2024-04-21 11:42:57 -0300

    mouse tbm interage

--- js/app.js
+++ js/app.js
@@ -4,10 +4,6 @@ document.getElementById('nome-amigo').focus();
 //exclui a lista de sugestão quando clica na caixa do Nome do Amigo
 document.getElementById('nome-amigo').setAttribute('autocomplete', 'off');
 
-//quando digitar qualquer coisa no campo: digite nome de um amigo, a tecla enter é habilitada
-document.getElementById('nome-amigo').addEventListener('input', function() {
-    document.getElementById('adicionar').removeAttribute('disabled');
-});
 
 //listas
 let amigosAdicionados = []
@@ -25,7 +21,6 @@ function adicionar() {
     document.getElementById('lista-amigos').innerText = amigosAdicionados.join(', ');
     document.getElementById('nome-amigo').value = ''; //apaga todo o campo depois de clicar enter
     console.log(amigosAdicionados);
-    document.getElementById('adicionar').setAttribute('disabled', 'true');//desabilita o botao 'adicionar' quando o campo principal está vazio
     } else {
         // Não há código aqui, então nada acontecerá se a variável estiver vazia, nula ou indefinida.
     }
@@ -63,11 +58,11 @@ document.addEventListener('keyup', function(teclar) {
     if (teclar.key === 'Enter') {
         teclar.preventDefault(); // Evita o comportamento padrão de submissão do formulário (evita atualizar a pagina
 
-        // caso o botao adicionar esteja desabilitado, a tecla enter irá interagir com o botao sortear
-        if (!document.getElementById('adicionar').hasAttribute('disabled')) {
-        adicionar(); //adicio o amigo digitado na lista 'amigosAdicionados' quando aperta o enter.
+        // caso a campo principal 'Digite nome de um amigo' esteja vazio, o botão adicionar não vai funcionar, passando a interação pro btao sortear
+        if (document.getElementById('nome-amigo').value == '') {
+        sortear(); //sorteia os nomes
         } else {
-        sortear();
+        adicionar();//adiciona o amigo digitado no campo 'amigos incluidos'
             }
     }
 });
@@ -81,6 +76,7 @@ function embaralharLista(amigosAdicionados) {
     console.log(amigosAdicionados);
     return amigosAdicionados;
 } 
+
 
 // document.getElementById('adicionar').setAttribute('disabled', 'true'); // Desabilita o botão "Adicionar" no início
 
